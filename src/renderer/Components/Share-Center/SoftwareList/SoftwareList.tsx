29b2eb2a72e86686be4b8d5c74a0ad7d14d9198a
Mohd Rejoan <wearecrx@gmail.com>
2023-03-11 12:40:24 +0600

    software list component created

--- src/renderer/Components/Share-Center/SoftwareList/SoftwareList.tsx
+++ src/renderer/Components/Share-Center/SoftwareList/SoftwareList.tsx
@@ -2,9 +2,41 @@ import React from 'react';
 import Cls from '../ShareCenter.module.scss';
 import Batch from 'renderer/Components/Ui/Batch/Batch';
 import SearchBar from 'renderer/Components/Ui/SearchBar/SearchBar';
-import { BiSearch } from 'react-icons/bi'
+import { BiSearch } from 'react-icons/bi';
+import { MdDelete, MdInstallDesktop } from 'react-icons/md';
+import { SiJirasoftware } from 'react-icons/si';
+
+const Software = ({ name }: { name: string }) => {
+  return (
+    <div className={Cls.softwareNode}>
+      <div className={Cls.softDetails}>
+        <SiJirasoftware size={20} />
+        <p className={Cls.name}>{name}</p>
+      </div>
+      <div className={Cls.controlBtn}>
+        <button className={Cls.install}>
+          <MdInstallDesktop size={20} />
+          <p>install</p>
+        </button>
+        <button className={Cls.delete}>
+          <MdDelete size={20} />
+          <p>delete</p>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const SoftwareList = () => {
+  const softList = [
+    {
+      name: 'MS Office 2010',
+    },
+    {
+      name: 'Photoshop 2016',
+    },
+  ];
+
   return (
     <div className={`${Cls.software} shadow ${Cls.cardPad}`}>
       <header>
@@ -20,8 +52,13 @@ const SoftwareList = () => {
           </button>
         </div>
       </header>
+      <div className={`${Cls.downSoftList}`}>
+        {softList.map((soft, index) => {
+          return <Software key={index} {...soft} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
-export default SoftwareList
+export default SoftwareList;
